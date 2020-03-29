1270e0706ca6d983b8823c1b1ce123f2b2718e2b
Tema Nikitin <megadestroyer95@gmail.com>
2020-03-29 18:30:53 +0300

    добавлена кнопка в футер

---
+++ script/booking.js
@@ -0,0 +1,4 @@
+let button = $('.booking__button')
+button.on('click', function() {
+    $('.booking__content').toggleClass('booking__content--opened')
+})
---
+++ script/header.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1) {
+        $('header').addClass("header--small");
+    } else {
+        $('header').removeClass("header--small");
+    }
+});
