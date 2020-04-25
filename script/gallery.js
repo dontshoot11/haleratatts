768f6ed31306d7d6cf5fd9ed4eaaf80b1a441edc
Tema Nikitin <megadestroyer95@gmail.com>
2020-04-25 13:45:37 +0300

    добавлена вторая галерея

--- script/gallery.js
+++ script/gallery.js
@@ -1,5 +1,41 @@
 let picture = $('.works-gallery__preview')
 picture.on('click', function() {
     $(this).toggleClass('works-gallery__preview--active');
-    console.log('клик')
+    let activePicture = $('.works-gallery__preview--active');
+    let prevPicture = activePicture.prev();
+    let nextPicture = activePicture.next();
+    let leftButton = $('.works-gallery__button--left');
+    let rightButton = $('.works-gallery__button--right');
+
+    if (prevPicture.length === 0) {
+        leftButton.css('visibility', 'hidden')
+    } else { leftButton.css('visibility', 'visible') };
+
+    if (nextPicture.length === 0) {
+        rightButton.css('visibility', 'hidden')
+    } else { rightButton.css('visibility', 'visible') };
+
+
+
+
+
+})
+
+let leftButton = $('.works-gallery__button--left')
+leftButton.on('click', function() {
+    console.log('куку');
+    let picture = $('.works-gallery__preview--active')
+    let prevPicture = picture.prev();
+
+
+    prevPicture.toggleClass('works-gallery__preview--active');
+
+})
+
+let rightButton = $('.works-gallery__button--right')
+rightButton.on('click', function() {
+    console.log('куку');
+    let picture = $('.works-gallery__preview--active')
+    let nextPicture = picture.next();
+    nextPicture.toggleClass('works-gallery__preview--active')
 })
