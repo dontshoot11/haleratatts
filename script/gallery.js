6bd428f771320c71b2ac2baff5a3855e97a0959a
Tema Nikitin <megadestroyer95@gmail.com>
2020-04-26 18:51:26 +0300

    переработаны две галереи

--- script/gallery.js
+++ script/gallery.js
@@ -1,5 +1,148 @@
-let picture = $('.works-gallery__preview')
-picture.on('click', function() {
+let button1 = $('.works-gallery__nav-button--sketches1');
+let button2 = $('.works-gallery__nav-button--sketches2');
+
+
+let firstGallery = $('.works-gallery__sketches .works-gallery__previews:first');
+let lastGallery = $('.works-gallery__sketches .works-gallery__previews:last');
+
+
+
+
+button1.on('click', function() {
+
+    let activeGallery = $('.works-gallery__sketches .works-gallery__previews--active');
+    let prevGallery = activeGallery.prev();
+
+    activeGallery.removeClass('works-gallery__previews--active');
+    prevGallery.addClass('works-gallery__previews--active');
+
+    if (firstGallery.hasClass('works-gallery__previews--active')) {
+        button1.css({ 'visibility': 'hidden', 'pointer-events': 'none' })
+    };
+    if (!lastGallery.hasClass('works-gallery__previews--active')) {
+        button2.css({ 'visibility': 'visible', 'pointer-events': 'auto' })
+    };
+
+
+
+
+
+
+});
+button2.on('click', function() {
+    let activeGallery = $('.works-gallery__sketches .works-gallery__previews--active');
+    let nextGallery = activeGallery.next();
+
+    activeGallery.removeClass('works-gallery__previews--active');
+    nextGallery.addClass('works-gallery__previews--active');
+
+
+    if (!firstGallery.hasClass('works-gallery__previews--active')) {
+        button1.css({ 'visibility': 'visible', 'pointer-events': 'auto' })
+    };
+    if (lastGallery.hasClass('works-gallery__previews--active')) {
+        button2.css({ 'visibility': 'hidden', 'pointer-events': 'none' })
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+let button3 = $('.works-gallery__nav-button--works1');
+let button4 = $('.works-gallery__nav-button--works2');
+
+
+
+
+
+
+
+
+button3.on('click', function() {
+    let firstGallery = $('.works-gallery__works .works-gallery__previews:first');
+    let lastGallery = $('.works-gallery__works .works-gallery__previews:last');
+
+    let activeGallery = $('.works-gallery__works .works-gallery__previews--active');
+    let prevGallery = activeGallery.prev();
+
+    activeGallery.removeClass('works-gallery__previews--active');
+    prevGallery.addClass('works-gallery__previews--active');
+
+    if (firstGallery.hasClass('works-gallery__previews--active')) {
+        button3.css({ 'visibility': 'hidden', 'pointer-events': 'none' })
+    };
+    if (!lastGallery.hasClass('works-gallery__previews--active')) {
+        button4.css({ 'visibility': 'visible', 'pointer-events': 'auto' })
+    };
+
+
+
+
+
+
+});
+button4.on('click', function() {
+    let firstGallery = $('.works-gallery__works.works-gallery__previews:first');
+    let lastGallery = $('.works-gallery__works .works-gallery__previews:last');
+    let activeGallery = $('.works-gallery__works .works-gallery__previews--active');
+    let nextGallery = activeGallery.next();
+
+    activeGallery.removeClass('works-gallery__previews--active');
+    nextGallery.addClass('works-gallery__previews--active');
+
+
+    if (!firstGallery.hasClass('works-gallery__previews--active')) {
+        button3.css({ 'visibility': 'visible', 'pointer-events': 'auto' })
+    };
+    if (lastGallery.hasClass('works-gallery__previews--active')) {
+        button4.css({ 'visibility': 'hidden', 'pointer-events': 'none' })
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+let pictures = $('.works-gallery__preview');
+
+
+
+pictures.on('click', function() {
+    event.preventDefault();
     $(this).toggleClass('works-gallery__preview--active');
     let activePicture = $('.works-gallery__preview--active');
     let prevPicture = activePicture.prev();
@@ -23,6 +166,7 @@ picture.on('click', function() {
 
 let leftButton = $('.works-gallery__button--left')
 leftButton.on('click', function() {
+    event.preventDefault();
     console.log('куку');
     let picture = $('.works-gallery__preview--active')
     let prevPicture = picture.prev();
@@ -34,6 +178,7 @@ leftButton.on('click', function() {
 
 let rightButton = $('.works-gallery__button--right')
 rightButton.on('click', function() {
+    event.preventDefault();
     console.log('куку');
     let picture = $('.works-gallery__preview--active')
     let nextPicture = picture.next();
